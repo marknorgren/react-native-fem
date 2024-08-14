a70ba070339c7cfa553c617ea595b6abcd4c25b4
Mark Norgren <mark@marknorgren.com>
2024-08-14 14:40:59 -0500

    fix submodules

---
+++ taskly/utils/storage.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function getFromStorage(key: string) {
+  try {
+    const data = await AsyncStorage.getItem(key);
+    console.log(`Get data from storage. Key: ${key}, Data: ${data}`);
+    return data ? JSON.parse(data) : null;
+  } catch {
+    console.log(`Error getting data from storage. Key: ${key}`);
+  }
+}
+
+export async function saveToStorage(key: string, data: object) {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(data));
+    console.log(`Save data from storage. Key: ${key}, Data: ${data}`);
+  } catch {
+    console.log(`Error saving data to storage. Key: ${key}, Data: ${data}`);
+  }
+}
